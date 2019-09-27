197591ac0b3adde5285d809a79abecd64f1f7f52
CaNplay <canplay@vip.qq.com>
2019-09-27 11:30:59 +0800

    #1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+  <q-page>
+    <q-card style="background-color: rgba(0, 0, 0, 0.5)">
+      <q-card-section>
+        <div class="q-gutter-md">
+          <q-input
+            standout="bg-teal text-white"
+            v-model="username"
+            label="用户名"
+            dark
+            bg-color="black"
+            style="opacity: 0.5"
+          />
+          <q-input
+            standout="bg-teal text-white"
+            v-model="password"
+            label="密码"
+            dark
+            bg-color="black"
+            style="opacity: 0.5"
+          />
+
+          <q-btn label="登录" color="primary" class="fit" @click="onLogin" />
+        </div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
